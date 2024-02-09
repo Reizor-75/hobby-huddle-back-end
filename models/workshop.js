6a4f05c65c85e9a7baf7bc0be92c2fdb49ee419a
SoundaryaNattuva <soundaryanatts@gmail.com>
2024-02-09 14:23:40 -0500

    create workshop model

---
+++ models/workshop.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const workshopSchema = new Schema({
+  mentorName: {
+    type: Schema.Types.ObjectId, 
+    ref: 'Profile'
+  },
+  eventType: {
+    required: true,
+    type: String,
+  }, 
+  date: {
+    required: true,
+    type: Date,
+  },
+  pricePerPerson: {
+    required: true,
+    type: Number,
+  },
+  location: {
+    type: Schema.Types.ObjectId, 
+    ref: 'Venue'
+  },
+  workshopLimit: {
+    required: true,
+    type: Number,
+  },
+  studentsAttending: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
+  description: {
+    required: true,
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['Craft', 'Art', 'Food', 'Sport', 'Music', 'Other']
+  }
+},{
+  timestamps: true,
+})
+
+const Workshop = mongoose.model('Workshop', workshopSchema)
+
+export { Workshop }
