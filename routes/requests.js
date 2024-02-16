9b5326596fe8e313d72bc5e96e1150024c2ac862
mlinares <m.linares93@gmail.com>
2024-02-15 22:29:58 -0500

    clean up routes

--- routes/requests.js
+++ routes/requests.js
@@ -12,9 +12,11 @@ router.post('/newRequest', auth.checkStudent, requestsCtrl.create)
 router.get('/', auth.checkMentor, requestsCtrl.index)
 router.get('/myRequests', auth.checkAuth, requestsCtrl.myRequest)
 router.put('/myRequests/:requestId', auth.checkStudent, requestsCtrl.update)
+//future feature
 router.put('/myRequests/:requestId/:bidId', auth.checkStudent, requestsCtrl.setAprroval)
 router.post('/:requestId', auth.checkMentor, requestsCtrl.createBid)
 router.get('/:requestId', auth.checkAuth, requestsCtrl.show)
+//future feature
 router.put('/:requestId/:bidId', auth.checkMentor, requestsCtrl.updateBid)
 router.delete('/:requestId', auth.checkAuth, requestsCtrl.delete)
 router.delete('/:requestId/:bidId', auth.checkMentor, requestsCtrl.deleteBid)
